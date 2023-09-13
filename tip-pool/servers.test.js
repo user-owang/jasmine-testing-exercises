1064d8d58b0ca4b9bb92cade3f7fcf3e5565afcb
unknown <hilow0090@gmail.com>
2023-09-12 17:55:42 -0700

    added clean up and additional tests to servers.test.js

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,24 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should add a new table row in the serverTable tbody for each server added', function() {
+    submitServerInfo();
+    serverNameInput.value = 'John';
+    submitServerInfo();
+
+    expect(document.querySelectorAll('#serverTable tbody tr').length).toEqual(2);
+  });
+
+  it('should add an id of server1 to the first row', function(){
+    submitServerInfo();
+
+    expect(document.querySelector('#serverTable tbody tr').id).toEqual('server1');
+  })
+
   afterEach(function() {
     // teardown logic
+    allServers = {};
+    serverId = 0;
+    updateServerTable();
   });
 });
